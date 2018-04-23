c15a534d150bf5a025f686afab5dbcc157e522ca
Dorian Amouroux <dor.amouroux@gmail.com>
2018-04-23 21:37:18 +0200

    add hot module reloading

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    ReactDOM.render(<NextApp />, document.getElementById("root"));
+  });
+}
