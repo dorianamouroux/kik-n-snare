a183412ad75f72fd33003ea40a220015840c6440
Dorian Amouroux <dor.amouroux@gmail.com>
2018-04-28 18:20:02 +0200

    styled progress track

--- src/components/track/progress-track.jsx
+++ src/components/track/progress-track.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 import range from "lodash.range";
+import styled from "styled-components";
 
-const baseStyle = {
-  width: 30,
-  height: 5,
-  border: "1px solid pink",
-  backgroundColor: "white"
-};
+const sizeBorderRadius = "3px";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  margin: 20px 0 40px;
+`;
+
+const Tile = styled.div`
+  width: ${100 / 16}%;
+  height: 5px;
+  border: 1px solid pink;
+  background-color: ${props => (props.current ? "white" : "pink")};
+
+  &:first-child {
+    border-radius: ${sizeBorderRadius} 0 0 ${sizeBorderRadius};
+  }
+
+  &:last-child {
+    border-radius: 0 ${sizeBorderRadius} ${sizeBorderRadius} 0;
+  }
+`;
 
 function ProgressTrackCell({ current }) {
-  const style = {
-    ...baseStyle,
-    ...(current ? { backgroundColor: "pink" } : {})
-  };
-  return <div style={style} />;
+  return <Tile current={current} />;
 }
 
 export default function ProgressTrack({ current }) {
   return (
-    <div style={{ display: "flex" }}>
+    <Container>
       {range(16).map(index => (
         <ProgressTrackCell key={index} current={index === current} />
       ))}
-    </div>
+    </Container>
   );
 }
