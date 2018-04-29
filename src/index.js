f8d20faec8ff778d1236a2e6341f5b8010bcaccd
Dorian Amouroux <dor.amouroux@gmail.com>
2018-04-29 14:42:26 +0200

    sentry error reporting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactGA from "react-ga";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Raven from "raven-js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -13,6 +14,12 @@ if (module.hot) {
 }
 
 if (process.env.NODE_ENV === "production") {
+  // track those users
   ReactGA.initialize("UA-73556931-2");
   ReactGA.pageview(window.location.pathname + window.location.search);
+
+  // error reporting
+  Raven.config(
+    "https://3ff695499b4f4e91adb08d30d98f6767@sentry.io/1198243"
+  ).install();
 }
